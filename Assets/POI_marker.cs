6ef8c6831d0e0a3c8b2517fc8d1527bf59ef34cc
Matt <potts.mat@gmail.com>
2016-04-23 23:07:40 +0100

    Added debug printing of debug info
    Single blue rectangle as a poi.
    name logged in debug.log

---
+++ Assets/POI_marker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class POI_marker : MonoBehaviour {
+	public string poi_name;
+
+	// Use this for initialization
+	void Start () {
+		poi_name = "Curiosity landing point";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

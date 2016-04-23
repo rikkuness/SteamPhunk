008fe486a2308d414920dddf9ee5e8a72b24edad
Dudley <pyratebeard@gmail.com>
2016-04-23 21:02:23 +0100

    MapShow script opened

---
+++ Assets/MapShow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using XInputDotNetPure;
+
+public class MapShow : MonoBehaviour {
+
+    public GameObject MapCanvas;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

fc6a657163d67499cb6988fe2b6c454a4e62b5aa
Dudley <pyratebeard@gmail.com>
2016-04-23 22:30:40 +0100

    random stuffs

--- Assets/MapShow.cs
+++ Assets/MapShow.cs
@@ -9,13 +9,13 @@ public class MapShow : MonoBehaviour {
 
     public GameObject MapCanvas; // Assign in inspector
     public bool ToggleMapOn;
-    public bool ToggleMapOff;
+    //public bool ToggleMapOff;
 
     // Use this for initialization
     void Start () {
         IsRunningOnMono = (Application.platform == RuntimePlatform.OSXEditor);
         ToggleMapOn = false;
-        ToggleMapOff = false;
+        //ToggleMapOff = false;
     }
 	
 	// Update is called once per frame
@@ -27,14 +27,12 @@ public class MapShow : MonoBehaviour {
             controlState = GamePad.GetState(PlayerIndex.One);
         }
 
-        if (ToggleMapOn == false && ToggleMapOff == false)
-        {
-            MapCanvas.SetActive(false);
-        }
-
         if (ToggleMapOn == true)
         {
             MapCanvas.SetActive(true);
+        } else
+        {
+            MapCanvas.SetActive(false);
         }
 
         if (((GamePadState)controlState).Buttons.Back == ButtonState.Pressed)
